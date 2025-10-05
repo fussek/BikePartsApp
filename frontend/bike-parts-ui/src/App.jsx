04f0053ad488d13d466e2c034aa258ca7c8d5aaa
patryk.fussek <patryk.fussek@julius-kuehn.de>
2025-10-05 23:42:44 +0200

    initial frontend setup

---
+++ frontend/bike-parts-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      </div>
+    </Router>
+  );
+}
+
+export default App;
